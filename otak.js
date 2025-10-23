7ac31d7a6ac6f32be6044af6bdbbae328a5dd757
Fahry Aditya Setiawan <Fahryadityagithub@gmail.com>
2025-10-23 08:12:33 +0800

    Delete otak.js

--- otak.js
+++
@@ -1,9 +0,0 @@
-// Ambil elemen tombol dan paragraf
-const tombol = document.getElementById("klikBtn");
-const pesan = document.getElementById("pesan");
-
-// Tambahkan event klik
-tombol.addEventListener("click", function() {
-  pesan.textContent = "Terima kasih sudah mengklik tombol ini! 😄";
-  pesan.style.color = "#0078d7";
-});
